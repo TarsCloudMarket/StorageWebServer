fd8715f027c899adf46004fbed407d21ed18fbc4
ruanshudong <ruanshudong@qq.com>
2022-08-18 11:21:57 +0800

    delete dist

--- dist/app/controller/PageController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class PageController {
-    static async index(ctx) {
-        // console.log("index", ctx);
-        await ctx.redirect("/index.html");
-    }
-}
-exports.default = PageController;
